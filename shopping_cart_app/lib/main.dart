880197289b4934ad47ac5ff4caee06defc8b9280
Ciro_Gronrroz <ciro.gronrroz@estudiantes.utec.edu.uy>
2024-03-04 23:15:39 -0300

    Firebase auth with flutter fire
    - simple user with email verification and its logic was added
    - folder refactor

--- shopping_cart_app/lib/main.dart
+++ shopping_cart_app/lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_cart_app/firebase_options.dart';
 import 'package:shopping_cart_app/home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
